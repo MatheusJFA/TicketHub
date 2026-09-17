31ff182c4a46be53cdbd2e332f60e3409171baff
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2026-09-17 18:29:28 -0300

    test: clean collections preserving indexes in ITs

--- server/infrastructure/src/test/java/com/tickethub/infrastructure/MongoCleanUpExtension.java
+++ server/infrastructure/src/test/java/com/tickethub/infrastructure/MongoCleanUpExtension.java
@@ -3,6 +3,7 @@ package com.tickethub.infrastructure;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.tickethub.infrastructure.audit.MongoAuditTrail;
@@ -13,6 +14,18 @@ public class MongoCleanUpExtension implements BeforeEachCallback {
     public void beforeEach(final ExtensionContext context) {
         final var applicationContext = SpringExtension.getApplicationContext(context);
         final var mongoTemplate = applicationContext.getBean(MongoTemplate.class);
-        mongoTemplate.dropCollection(MongoAuditTrail.COLLECTION);
+        cleanCollections(mongoTemplate, MongoAuditTrail.COLLECTION);
+    }
+
+    public static void cleanCollections(final MongoTemplate mongoTemplate, final String... collections) {
+        for (final String collection : collections) {
+            mongoTemplate.remove(new Query(), collection);
+        }
+    }
+
+    public static void dropCollections(final MongoTemplate mongoTemplate, final String... collections) {
+        for (final String collection : collections) {
+            mongoTemplate.dropCollection(collection);
+        }
     }
 }
